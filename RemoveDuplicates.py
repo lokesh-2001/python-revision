dbb92fa497f4cd6aa981a85807e3dc12e988073d
Lokesh Dhingra <lokesh2001dhingra@gmail.com>
2021-12-30 23:28:11 +0530

    opractice part 3

---
+++ RemoveDuplicates.py
@@ -0,0 +1,4 @@
+# Write a program that takes a list and returns a new list that contains 
+# all the elements of the first list minus all the duplicates.
+a = [1,2,3,4,5,7,1,25,2,5,1]
+print(list(set(a)))
---
+++ reverseWordOrder.py
@@ -0,0 +1,14 @@
+# Write a program (using functions!) that asks the user for a 
+# long string containing multiple words. Print back to the user 
+# the same string, except with the words in backwards order. 
+# For example, say I type the string:
+
+#   My name is Michele
+# Then I would see the string:
+
+#   Michele is name My
+
+sent = input("Enter sentence: ")
+y = sent.split()
+sent = " ".join(y[::-1])
+print(sent)
